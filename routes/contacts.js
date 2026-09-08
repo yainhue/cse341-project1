bed6e30f090eb98e872fa41e590e7b35a9107895
yainhue <huentoyain4iaa@gmail.com>
2026-09-07 21:38:41 -0300

    Structure Fix

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,10 +5,6 @@ const contactsControllers = require("../controllers/contacts");
 
 router.get("/", contactsControllers.getAll);
 
-// router.get("/", (req, res) => {
-//     res.send("Lista de contactos desde contacts.js");
-// });
-
 router.get("/:id", contactsControllers.getSingle);
 
 module.exports = router;
